9c8857f61076d4f08921ec806e610f52432a9e11
Masaki Muranaka <monaka@monami-ya.jp>
2013-04-21 14:11:03 +0900

    Use stdint.h

--- types.h
+++ types.h
@@ -1,13 +1,14 @@
 #ifndef _TYPES_H_
 #define _TYPES_H_
 
+#include <stdint.h>
 
-typedef unsigned long UInt32;
-typedef signed long Int32;
-typedef unsigned short UInt16;
-typedef signed short Int16;
-typedef unsigned char UInt8;
-typedef signed char Int8;
+typedef uint32_t UInt32;
+typedef int32_t Int32;
+typedef uint16_t UInt16;
+typedef int16_t Int16;
+typedef uint8_t UInt8;
+typedef int8_t Int8;
 typedef unsigned char Err;
 typedef unsigned char Boolean;
 
