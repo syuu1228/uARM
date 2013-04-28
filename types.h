558177c31203b403cc130159e8084809fc00bf01
MAEKAWA Masahide <cvsync.mm@gmail.com>
2013-04-28 17:00:39 +0900

    Support Big-Endian system.

--- types.h
+++ types.h
@@ -44,5 +44,56 @@ void __mem_copy(void* d, const void* s, UInt32 sz);
 #endif
 #define memcpy __memcpy_disabled__
 
+#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
+#include <sys/endian.h>
+#endif /* defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) */
+
+#ifndef LITTLE_ENDIAN
+#define	LITTLE_ENDIAN	(0xAABBCCDDUL)
+#endif /* LITTLE_ENDIAN */
+#ifndef BIG_ENDIAN
+#define	BIG_ENDIAN	(0xDDCCBBAAUL)
+#endif /* BIG_ENDIAN */
+
+#if defined(__ppc__)
+#ifndef BYTE_ORDER
+#define	BYTE_ORDER	BIG_ENDIAN
+#endif /* BYTE_ORDER */
+#endif /* defined(__ppc__) */
+
+/* default byte order is little (same as original) */
+#ifndef BYTE_ORDER
+#define	BYTE_ORDER	LITTLE_ENDIAN
+#endif /* BYTE_ORDER */
+
+#if BYTE_ORDER == LITTLE_ENDIAN
+#ifndef le32toh
+#define	le32toh(x)	(x)
+#endif /* le32toh */
+#ifndef le16toh
+#define	le16toh(x)	(x)
+#endif /* le16toh */
+#endif /* BYTE_ORDER == LITTLE_ENDIAN */
+
+#if BYTE_ORDER == BIG_ENDIAN
+#ifndef le32toh
+#define	le32toh(x)	((((x) << 24) & 0xFF000000UL) | \
+			 (((x) <<  8) & 0x00FF0000UL) | \
+			 (((x) >>  8) & 0x0000FF00UL) | \
+			 (((x) >> 24) & 0x000000FFUL))
+#endif /* le32toh */
+#ifndef le16toh
+#define	le16toh(x)	((((x) << 8) & 0xFF00U) | \
+			 (((x) >> 8) & 0x00FFU))
+#endif /* le16toh */
+#endif /* BYTE_ORDER == BIG_ENDIAN */
+
+#ifndef htole32
+#define	htole32		le32toh
+#endif /* htole32 */
+#ifndef htole16
+#define	htole16		le16toh
+#endif /* htole16 */
+
 #endif
 
